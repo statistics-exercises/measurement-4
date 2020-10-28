ed75b6d73b34aa4ad5ce6061c506d4f25eab8acb
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-28 18:50:37 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# I have loaded the radii of all the bubbles that are in 
+# the file here
+radii = np.loadtxt("bubbles.dat")
+
+# Your code to plot all the volumes goes here
